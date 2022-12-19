470eace3d33e712923d04382d35bcbd2bdf4bcd1
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2022-12-19 07:16:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const lib = require('lib')({token: process.env['ltoken']});
 const Discord = require("discord.js");
-const client = new Discord.Client();
+//const client = new Discord.Client();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 const config = require("./config.json");
 
 client.on("ready", ()=>console.log("READY"));
